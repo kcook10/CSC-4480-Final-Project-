48d3dcb2bca39c7e58eae864505c9eb6113bd208
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-12-04 23:34:34 -0500

    Update Restaurant_query.sql

--- Restaurant_query.sql
+++ Restaurant_query.sql
@@ -1,1 +1,5 @@
-
+/*List the people who work in the Seattle Restaurant and order their hours in descending order */
+SELECT DISTINCT E.Fname, E.Lname, E.HoursWorked, R.Location
+FROM EMPLOYEES E, RESTAURANT R
+WHERE R.LOCATION = 'Seattle'
+ORDER BY E.HoursWOrked Desc;

0076c812339d5bccd3461d2ece076c91c1279abc
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-12-04 23:36:10 -0500

    Update Restaurant_query.sql

--- Restaurant_query.sql
+++ Restaurant_query.sql
@@ -2,4 +2,9 @@
 SELECT DISTINCT E.Fname, E.Lname, E.HoursWorked, R.Location
 FROM EMPLOYEES E, RESTAURANT R
 WHERE R.LOCATION = 'Seattle'
-ORDER BY E.HoursWOrked Desc;
+ORDER BY E.HoursWorked Desc;
+
+/*List customers whose first name starts with A*/
+SELECT Fname, Lname
+FROM CUSTOMER
+WHERE Fname LIKE 'A%';

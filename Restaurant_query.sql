6d58c1b6a65e56ab2daf0f1b9e9c072219ccc5cb
alexaper12 <130948432+alexaper12@users.noreply.github.com>
2023-12-04 23:41:33 -0500

    Update Restaurant_query.sql

--- Restaurant_query.sql
+++ Restaurant_query.sql
@@ -9,16 +9,20 @@ SELECT Fname, Lname
 FROM CUSTOMER
 WHERE Fname LIKE 'A%';
 
-/*List the first and last names of all customers who ordered to the Orlando or Boston restaurant*/
+/*List the first and last names of all customers who ordered to the Orlando or Boston Restaurant*/
 SELECT DISTINCT C.Fname, C.Lname
 FROM CUSTOMER C
 WHERE C.CustomerID IN
-  (SELECT O.CustomerID
-  FROM ORDERS O
-  JOIN RESTAURANT R ON O.RestaurantID = R.RestaurantID
-  WHERE R.Location = 'Orlando')
-OR C.CustomerID IN
-  (SELECT O.CustomerID
-  FROM ORDERS O
-  JOIN RESTAURANT R ON O.RestaurantID = R.RestaurantID
-  WHERE R.Location = 'Boston');
+    (SELECT O.CustomerID
+    FROM ORDERS O
+    JOIN RESTAURANT R ON O.RestaurantID = R.RestaurantID
+    WHERE R.Location = 'Orlando')
+    OR C.CustomerID IN
+    (SELECT O.CustomerID
+    FROM ORDERS O
+    JOIN RESTAURANT R ON O.RestaurantID = R.RestaurantID
+    WHERE R.Location = 'Boston');
+
+/*Insert a table number out of the constraint*/
+INSERT INTO ORDERS (OrderID, Price, Times, Order_Type, EmployeeID, Dates, Table_num, CustomerID, RestaurantID)
+VALUES (4952, 11.00, '2:33', 'Here', 13810408, '27-OCT-23', 23, 58245964, 33028090);
